1d72a605253ef32510d1bd4949726a022a10693e
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2020-02-15 23:56:04 +0900

    Create Class1.cs

---
+++ 2020_02_10/nsh/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        string T = Console.ReadLine();
+
+        int[] I = Aorder(T);
+        List<char> list = new List<char>();
+
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+            char[] C = ROW.ToCharArray();
+
+            for (int j = 0 ; j < C.Length ; j++){
+                list.Add(C[j]);
+            }
+        }
+        for(int k = 0; k < H ;k++){
+            for (int i = 0; i < I.Length; i++){
+                for (int j = 0; j < L; j++){
+                    Console.Write(list[(L * I[i]) + j + (k* (L*27) )  ]);
+                }
+            }Console.WriteLine();
+        }
+    }
+    public static int[] Aorder(string T)
+    {
+        char[] C = T.ToCharArray();
+        int[] I = new int[C.Length];
+
+        for(int i = 0; i < C.Length ; i++)
+        {
+            if (Convert.ToInt32(C[i]) >= 65 && Convert.ToInt32(C[i]) <= 90)
+                I[i] = Convert.ToInt32(C[i]) - 65;
+
+            else if (Convert.ToInt32(C[i]) >= 97 && Convert.ToInt32(C[i]) <= 122)
+                I[i] = Convert.ToInt32(C[i]) - 97;
+
+            else I[i] = 26;
+        }
+        return I;
+    }
+}
